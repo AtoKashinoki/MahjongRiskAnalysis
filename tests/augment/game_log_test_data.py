5b7b9095b5407883b3cc6fb299baba21f12cb52a
AtoKashinoki <atokashinoki@gmail.com>
2025-11-30 12:59:21 +0900

    Update augmenter test codes

--- tests/augment/game_log_test_data.py
+++ tests/augment/game_log_test_data.py
@@ -28,35 +28,40 @@ TRAINING_DATAS = os.path.join(DIST, "test_datas")
 
 
 if __name__ == '__main__':
-    for year in TARGET_YEARS:
-        target_game_logs = os.path.join(GAME_LOGS, year)
-        target_training_datas = os.path.join(
-            TRAINING_DATAS,
-            f"model{AugmenterConfig.model_id}",
-            year
-        )
+    for model_id in range(1, 3 + 1):
+        AugmenterConfig.model_id = model_id
 
-        listdir = [
-            filename
-            for filename in os.listdir(target_game_logs)
-            if filename[4:8] == year
-        ]
-        listdir_length = len(listdir)
-
-        for idx, filename in enumerate(listdir):
-            print(f"Augmenting: {filename}")
-            save_file_path = os.path.join(
-                target_training_datas,
-                filename.replace("log", "training_data")
+        for year in TARGET_YEARS:
+            target_game_logs = os.path.join(GAME_LOGS, year)
+            target_training_datas = os.path.join(
+                TRAINING_DATAS,
+                f"model{AugmenterConfig.model_id}",
+                year
             )
-            target = os.path.join(target_game_logs, filename)
-            result = augment_and_save_game_log(target, save_file_path, overwrite=True)
-            if result is None:
-                print(f"File {save_file_path} already exists, skipping.")
+
+            listdir = [
+                filename
+                for filename in os.listdir(target_game_logs)
+                if filename[4:8] == year
+            ]
+            listdir_length = len(listdir)
+
+            for idx, filename in enumerate(listdir):
+                print(f"Augmenting: {filename}")
+                save_file_path = os.path.join(
+                    target_training_datas,
+                    filename.replace("log", "training_data")
+                )
+                target = os.path.join(target_game_logs, filename)
+                result = augment_and_save_game_log(target, save_file_path)
+                if result is None:
+                    print(f"File {save_file_path} already exists, skipping.")
+                    continue
+                print("Augmented log saved to " + save_file_path)
+                progress_num = idx + 1
+                print(f"Progress: {progress_num * 100 / listdir_length:.2f}%[{progress_num}/{listdir_length}]")
                 continue
-            print("Augmented log saved to " + save_file_path)
-            progress_num = idx+1
-            print(f"Progress: {progress_num*100/listdir_length:.2f}%[{progress_num}/{listdir_length}]")
+
             continue
 
         continue
